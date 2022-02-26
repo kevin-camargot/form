4d64a628b50f0329a3968eb04ee5bd7a87e83cbb
kevin.camargot <kevin.camargo@theskimm.com>
2022-02-26 09:38:41 -0500

    add useEffect

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,32 @@
+import React, { useState, useEffect } from 'react'
 import Head from 'next/head'
 import Header from '../components/header'
 import { DatePicker } from 'antd';
 
 export default function Home() {
+
+  const [info, setInfo] = useState("Info")
+  /* 
+    useEffect(() => {
+      console.log("sad")
+      fetch('https://jsonplaceholder.typicode.com/todos/1')
+        .then(response => response.json())
+        .then(json => console.log(json))
+    }, [])
+   */
+  useEffect(() => {
+    setInfo("InfoSETEADA")
+    fetch('https://jsonplaceholder.typicode.com/todos')
+      .then(response => response.json())
+      .then(json => console.log(json))
+  }, [])
+
+
+
+
   return (
-    <div className="app">
+
+    < div className="app" >
       <Head>
         <title>This page has for form test</title>
         <meta charSet="utf-8" />
@@ -12,14 +34,14 @@ export default function Home() {
       </Head>
       <Header />
 
-      (<DatePicker />
+      <DatePicker />
 
 
       <form action="#" method='POST'>
-        <label>nombre</label>
+        <label>nombre {info}</label>
         <input type="text" placeholder="correo@mysite.com"></input>
         <button type="submit">Enviar</button>
       </form>
-    </div>
+    </div >
   )
 }
